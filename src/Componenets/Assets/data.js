e8ecc5b23f9feac3fc6f615c4d61adc5c07351fb
barathsaki2 <barathsaki2244@gmail.com>
2023-11-21 16:05:58 +0530

    first commit

---
+++ src/Componenets/Assets/data.js
@@ -0,0 +1,26 @@
+import p1_img from './product_1.png'
+import p2_img from './product_2.png'
+import p3_img from './product_3.png'
+import p4_img from './product_4.png'
+
+let data_product = [
+  {
+    id:1,
+    name:"Children Care",
+    //image:p1_img,
+  },
+  {id:2,
+    name:"Cooking",
+    //image:p2_img,
+  },
+  {id:3,
+    name:"Home Cleaning",
+    //image:p3_img,
+  },
+  {id:4,
+    name:"Garden cleaning",
+    // image:p4_img,
+  },
+];
+
+export default data_product;
